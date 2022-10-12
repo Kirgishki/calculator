ec795d2184f1e2414ac8d98dd471f1aa2f9baf74
Jovan_Obrencevic <jovan.obrencevic34@gmail.com>
2022-10-12 11:31:00 +0200

    Added functions for basic math operators

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function add(firstNum, secNum){
+    return Number(firstNum) + Number(secNum);
+}
+
+function subtract(firstNum, secNum){
+    return Number(firstNum) - Number(secNum);
+}
+
+function multiply(firstNum, secNum){
+    return Number(firstNum) * Number(secNum);
+}
+
+function divide(firstNum, secNum){
+    return Number(firstNum) / Number(secNum);
+}
+
+function operate(operator, firstNum, secNum){
+    switch (operator) {
+        case "+":
+            return add(firstNum, secNum);
+        case "-":
+            return subtract(firstNum, secNum);
+        case "*":
+            return multiply(firstNum, secNum);
+        case "/":
+            return divide(firstNum, secNum);
+        default:
+            break;
+    }
+}
+
